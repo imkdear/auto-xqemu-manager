231348caffed3f9393f367ce9355f3431f7918e5
imkdear <69320484+imkdear@users.noreply.github.com>
2020-08-14 14:08:33 +0200

    Update build-xqemu-manager.sh

--- build-xqemu-manager.sh
+++ build-xqemu-manager.sh
@@ -1,6 +1,7 @@
 git clone -q --branch=master https://github.com/xqemu/xqemu-manager.git
 cd xqemu-manager
 python -c "import struct; print(struct.calcsize('P') * 8)"
+sudo apt install python3-pip
 python3 -m pip install --upgrade pip
 pip install PyQt5 PyInstaller
 ~/.local/bin/pyinstaller --onefile  --windowed --name xqemu-manager main.py
